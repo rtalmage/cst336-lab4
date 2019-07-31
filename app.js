108c62b5cdc458737079e099d588aa0783c05100
Raymond Talmage <raymondtalmage@gmail.com>
2019-07-30 18:24:53 -0700

    Initial commit for solar system site

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+    res.render("index.html");
+});
+
+app.get("/mercury", function(req, res){
+    res.render("mercury.html");
+});
+
+app.get("/venus", function (req,res) {
+    res.send("This will be Venus web page")
+});
+
+//server listener
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Express Server is Running...")
+});
